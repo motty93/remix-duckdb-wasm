74b64989638bcaf8fe92e2a3645203c154edc494
motty93 <rdwbocungelt5@gmail.com>
2025-03-26 09:50:12 +0900

    refactor: remove unused imports and comments in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { HeadersFunction, LinksFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import './tailwind.css';
 
 export const links: LinksFunction = () => [];
@@ -30,7 +30,6 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        {/* ブラウザのHTMLレベルでもCSPを設定 */}
         <meta
           httpEquiv='Content-Security-Policy'
           content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:; worker-src 'self' blob:; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data:; connect-src 'self' blob: ws: wss:;"
@@ -43,7 +42,6 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
